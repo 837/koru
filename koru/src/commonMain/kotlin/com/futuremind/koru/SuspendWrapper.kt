87c45f50ec242904d00d9e0f6bb91033a43322e9
ern <ern@ti8m.ch>
2021-06-25 09:53:52 +0200

    Revert "throw Exception in onThrow of subscribe"
    This reverts commit 6e2c74a22614aa975c9f6f49eee3f9d34a9fd015.

--- koru/src/commonMain/kotlin/com/futuremind/koru/SuspendWrapper.kt
+++ koru/src/commonMain/kotlin/com/futuremind/koru/SuspendWrapper.kt
@@ -9,29 +9,27 @@ class SuspendWrapper<T>(
     private val scopeProvider: ScopeProvider?,
     private val suspender: suspend () -> T
 ) {
+
     fun subscribe(
         onSuccess: (item: T) -> Unit,
         onThrow: (error: Throwable) -> Unit
     ) = subscribe(
-        scope = scopeProvider?.scope,
+        scope = scopeProvider?.scope
+            ?: throw IllegalArgumentException("To use implicit scope, you have to provide it via @ToNativeClass.launchOnScope and @ExportedScopeProvider."),
         onSuccess = onSuccess,
         onThrow = onThrow
     )
 
     fun subscribe(
-        scope: CoroutineScope?,
+        scope: CoroutineScope,
         onSuccess: (item: T) -> Unit,
         onThrow: (error: Throwable) -> Unit
-    ): Job?  {
-        if(scope==null){
-            onThrow(IllegalArgumentException("To use implicit scope, you have to provide it via @ToNativeClass.launchOnScope and @ExportedScopeProvider."))
+    ): Job = scope.launch {
+        try {
+            onSuccess(suspender().freeze())
+        } catch (error: Throwable) {
+            onThrow(error.freeze())
         }
-        return scope?.launch {
-            try {
-                onSuccess(suspender().freeze())
-            } catch (error: Throwable) {
-                onThrow(error.freeze())
-            }
-        } 
-    } 
+    }
+
 }
